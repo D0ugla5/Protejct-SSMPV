b2dc5a62a3b497b253b40a353cae9bfd4d5d3d21
Douglas Aurélio DE JESUS <163046084+D0ugla5@users.noreply.github.com>
2024-11-21 16:30:41 -0300

    Added a functional login
    YEs it's working but is just the start
    i need to connect with my mongo yet.

---
+++ src/api/user/dto/user.dto.ts
@@ -0,0 +1,27 @@
+import { 
+  IsString,
+  IsEmail,
+  IsStrongPassword,
+  IsNotEmpty,
+  IsDateString
+} from "class-validator";
+
+
+
+export class UserDto{
+  @IsNotEmpty()
+  @IsString()
+  name:string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  birthday:string;
+  
+  @IsNotEmpty()
+  @IsEmail()
+  email:string;
+  
+  @IsNotEmpty()
+  @IsStrongPassword()
+  password:string;
+}
