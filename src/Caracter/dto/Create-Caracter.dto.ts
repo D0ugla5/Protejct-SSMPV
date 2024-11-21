3ab795b042410149087a2de4ab7f5a834a30ef55
Douglas Aurélio DE JESUS <163046084+D0ugla5@users.noreply.github.com>
2024-11-21 16:28:26 -0300

    Delete src directory

--- src/Caracter/dto/Create-Caracter.dto.ts
+++
@@ -1,7 +0,0 @@
-export class Caracter {
-    nome:string;
-    apelido?:string;
-    birthday:Date;
-    password:string|number;
-    
-}
--- src/module/sugestions/md¹.md
+++
@@ -1,1 +0,0 @@
-Here it must be the first page that would be a interect system
